be4ffd9bbf6893828ee2d9f60163e5e44a793480
VincentL0L <hivincentwlo@gmail.com>
2025-06-24 12:39:15 -0700

    Fix typo in MenuScene filename and update script type

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 import MenuScene from './scenes/MenuScene.js';
-import MapScene from './scenes/MapScene.js';
-import Scene1 from './scenes/Scene1.js';
-import Scene2 from './scenes/Scene2.js';
-import Scene3 from './scenes/Scene3.js';
-import CameraScene from './scenes/CameraScene.js';
-import CameraScene2 from './scenes/CameraScene2.js';
 
 const config = {
   type: Phaser.AUTO,
@@ -12,18 +6,10 @@ const config = {
   height: 982,
   backgroundColor: '#000000',
   parent: 'game-container',
-  scene: [
-    MenuScene,
-    Scene1,
-    Scene2,
-    Scene3,
-    MapScene,
-    CameraScene,
-    CameraScene2
-  ],
+  scene: [MenuScene],
   scale: {
     mode: Phaser.Scale.RESIZE,
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    autoCenter: Phaser.Scale.CENTER_BOTH,
   }
 };
 
